493b45828d4c683cb5acb9d04c8a7f0b8f964d1d
simao-tavares <simaotavares1@proton.me>
2026-06-26 19:12:14 +0100

    feat: configure main application and error handlers

---
+++ app.py
@@ -0,0 +1,34 @@
+import os
+from flask import Flask, jsonify
+from extensions import db, jwt
+from routes.auth import auth
+from dotenv import load_dotenv
+
+
+load_dotenv()
+
+app = Flask(__name__)
+
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///todo-list.db'
+app.config["JWT_SECRET_KEY"] = os.environ.get('JWT_SECRET_KEY')
+
+db.init_app(app)
+jwt.init_app(app)
+
+app.register_blueprint(auth)
+
+@app.errorhandler(405)
+def handle_405(e):
+    return jsonify({'message': 'Method not allowed for this endpoint'}), 405
+
+@app.errorhandler(415)
+def handle_415(e):
+    return jsonify({'message': 'Request body is empty'}), 415
+
+
+if __name__ == '__main__':
+    with app.app_context():
+        db.create_all()
+
+    app.run(debug = True)
+
