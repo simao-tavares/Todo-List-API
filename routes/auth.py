e3d54617fddea081c55fa92358992381b059349b
simao-tavares <simaotavares1@proton.me>
2026-06-27 12:52:20 +0100

    feat: implement user login endpoint

--- routes/auth.py
+++ routes/auth.py
@@ -1,7 +1,6 @@
 from flask import Blueprint, jsonify, request
 from werkzeug.exceptions import BadRequest
 from schemas.validation import validate_json
-from services.todo_services import TodoServices
 from services.user_services import UserServices
 from tokens import create_token
 
@@ -41,7 +40,39 @@ def register():
         return jsonify({'message': 'Internal server error'}), 500
 
     return jsonify({'token': create_token(user_id)}), 200
-        
+
+
+@auth.route('/login', methods = ['POST'])
+def login():
+    try:
+        data = request.json
+        validation = validate_json(data, 'Login')
+        if isinstance(validation, dict):
+            return jsonify(validation), 400
+    except BadRequest:
+        return jsonify({'message': 'Invalid JSON format'}), 400
+       
+    email = data['email']
+    password = data['password']
+
+    user_services = UserServices()
+
+    email_exists = user_services.find_user_by_email(email)
+
+    if not email_exists:
+        return jsonify({'message': 'Email or password are wrong'}), 401
+
+    if email_exists is None:
+        return jsonify({'message': 'Internal server error'}), 500
+
+    db_user_id, db_password = user_services.get_id_and_password(email)
+
+    is_valid = user_services.compare_passwords(db_password, password)
+
+    if not is_valid:
+        return jsonify({'message': 'Email or password are wrong'}), 401
+
+    return jsonify({'token': create_token(db_user_id)}), 200
 
     
 
