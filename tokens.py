eda7f3fa4ec699fc80d42e88b40cc61c19642492
simao-tavares <simaotavares1@proton.me>
2026-06-27 21:22:12 +0100

    feat: implement function to validate tokens

--- tokens.py
+++ tokens.py
@@ -1,4 +1,13 @@
-from flask_jwt_extended import create_access_token
+from flask_jwt_extended import create_access_token, decode_token
+from jwt import ExpiredSignatureError, DecodeError, InvalidTokenError
 
 def create_token(id: int):
-    return create_access_token(identity = id)
+    return create_access_token(identity = str(id))
+
+def validate_token(token: str):
+    try:
+        token_decoded = decode_token(token)
+        return token_decoded['sub']
+    except (ExpiredSignatureError, DecodeError, InvalidTokenError) as e:
+        return e
+        
