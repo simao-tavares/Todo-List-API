1964c86b0ea8e6eaefea55d10b15d79bc9f183af
simao-tavares <simaotavares1@proton.me>
2026-06-28 13:23:45 +0100

    feat: implement todo delete endpoint

--- routes/todo.py
+++ routes/todo.py
@@ -105,8 +105,49 @@ def update_or_delete(id):
             return jsonify({'message': 'Internal server error'}), 500
 
         return jsonify({'id': id, 'title': updated_title, 'description': updated_description}), 200
-        
 
+    if request.method == 'DELETE':
+        header = request.headers.get('Authorization')
+
+        if header is None:
+            return jsonify({'message': 'Unauthorized'}), 401
+
+        user_id = validate_token(header.split(' ')[1])
+
+        if user_id is None:
+            return jsonify({'message': 'Unauthorized'}), 401
+
+        user_services = UserServices()
+
+        user_exists = user_services.find_user_by_id(user_id)
+
+        if not user_exists:
+            return jsonify({'message': 'Unauthorized'}), 401
+
+        if user_exists is None:
+            return jsonify({'message': 'Internal server error'}), 500
+        
+        todo_services = TodoServices()
+
+        todo_exists = todo_services.find_todo_by_id(id)
+
+        if todo_exists is None:
+            return jsonify({'message': 'Internal server error'}), 500
+
+        if not todo_exists:
+            return jsonify({'message': f'Task with id {id} not found'}), 404
+
+        todo_user_id = todo_services.get_todo_userid(id)
+
+        if int(user_id) != int(todo_user_id):
+            return jsonify({'message': 'Forbidden'}), 403
+
+        delete_todo = todo_services.delete_todo(id)
+
+        if delete_todo is None:
+            return jsonify({'message': 'Internal server error'}), 500
+
+        return '', 204
         
 
         
