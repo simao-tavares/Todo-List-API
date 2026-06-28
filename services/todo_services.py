0b43f85ef1eb46d791021cca7ea5162e1dc82b0f
simao-tavares <simaotavares1@proton.me>
2026-06-28 13:11:44 +0100

    feat: implement functions to find, get and update todo

--- services/todo_services.py
+++ services/todo_services.py
@@ -5,7 +5,7 @@ from sqlalchemy.exc import InvalidRequestError, OperationalError
 
 class TodoServices:
 
-    def create_todo(self, title: str, user_id: int,  description: str = '') -> int | None:
+    def create_todo(self, title: str, description: str, user_id: int) -> int | None:
         try:
             add = Todo(title = title, description = description, user_id = user_id)
             db.session.add(add)
@@ -14,6 +14,40 @@ class TodoServices:
         except (InvalidRequestError, OperationalError, TimeoutError):
             db.session.rollback()
             return
+
+    def find_todo_by_id(self, id: int):
+        try:
+            search = db.session.query(Todo).filter(Todo.id == id).first()
+            if search is None:
+                return False
+            else:
+                return True
+        except (InvalidRequestError, OperationalError, TimeoutError):
+            db.session.rollback()
+            return
+
+    def get_todo_userid(self, id: int):
+        try:
+            search = db.session.query(Todo).filter(Todo.id == id).first()
+            if search is None:
+                return False
+            else:
+                print(search)
+                return search.user_id
+        except (InvalidRequestError, OperationalError, TimeoutError):
+            db.session.rollback()
+            return
+
+    def update_todo(self, id: int, title: str, description: str):
+        try:
+            search = db.session.query(Todo).filter(Todo.id == id).first()
+            search.title = title
+            search.description = description
+            db.session.commit()
+            return search.title, search.description
+        except (InvalidRequestError, OperationalError, TimeoutError):
+            db.session.rollback()
+            return   
 
 
 
